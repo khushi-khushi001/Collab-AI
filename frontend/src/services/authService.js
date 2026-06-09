725f526e88161a9d8097bcde1607297934fd5125
khushi <kv217471@gmail.com>
2026-06-09 09:44:33 +0530

    fix production url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 // signup
 export const signupUser = async(userData) => {
@@ -11,7 +11,7 @@ export const signupUser = async(userData) => {
 
 // login 
 export const loginUser = async (userData) => {
-    const response = await axios.post(`${API_URL}//login`, userData);
-    console.log("api url:", `${API_URL}/login`);
+    const response = await axios.post(`${API_URL}/api/auth/login`, userData);
+    console.log("api url:", `${API_URL}/api/auth/login`);
     return response.data;
 };
